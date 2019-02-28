b37109d77320da2b4819d90ef6657a5b376b6aec
Zayd Algawi <Zayd.Algawi@hotmail.co.uk>
2019-02-28 15:45:05 +0000

    First edition of the project

---
+++ dissertationProj/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dissertationProj.Startup))]
+namespace dissertationProj
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
